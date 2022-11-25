0e9ef2019cf518f7f86bbbb0bb25161c055cfcc4
Schwannden Kuo <schwannden@mobagel.com>
2022-11-25 17:25:53 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/schwannden/greetings
+
+go 1.19
---
+++ greetings.go
@@ -0,0 +1,8 @@
+package greetings
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("hi, %v. Welcome!", name)
+	return message
+}
